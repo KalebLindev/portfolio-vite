1daad6ec7c49d362651610ea3130ea4e12f9a403
KalebLindev <40879958+KalebLindev@users.noreply.github.com>
2023-04-04 19:13:54 -0400

    added countryAPI and RedditClone project links

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,17 @@
-const ProjectCard = ({img, title, text}) => {
+import { useNavigate } from "react-router"
+
+const ProjectCard = ({img, title, text , projectLink = null}) => {
+
+    const navigate = useNavigate()
 
     const handleClick = () => {
-        alert('Title: ' + title)
+        console.log(projectLink)
+        return window.open(projectLink)
     }
 
     return (
-            <div className="project-card" onClick={handleClick}>
-                <img src={img}/>
+            <div className="project-card">
+                <img src={img} onClick={handleClick}/>
                 <div className="project-info">
                     <h3>{title}</h3>
                     <p>{text}</p>
--- src/components/WebDev.jsx
+++ src/components/WebDev.jsx
@@ -1,13 +1,14 @@
-import img1 from '../images/encounter/encounter-1.jpeg'
 import ProjectCard from './ProjectCard'
+import countryAPIImg from '../images/projectLandingPages/femcountryapi.png'
+import redditCloneImg from '../images/projectLandingPages/redditclone.png'
 
 const WebDev = () => {
     return (
         <div className="webdev-container" >
             <div className="webdev">
-                <ProjectCard img={img1} title="NAME" text="Info on the project Info on the project Info on the project Info on the project Info on the project"/>
-                <ProjectCard img={img1} title="NAME" text="Info on the project Info on the project Info on the project Info on the project Info on the project"/>
-                <ProjectCard img={img1} title="NAME" text="Info on the project Info on the project Info on the project Info on the project Info on the project"/>
+                <ProjectCard img={countryAPIImg} title="Country API" text="Search and filter countries" projectLink="https://femcountry.netlify.app/"/>
+                <ProjectCard img={redditCloneImg} title="Reddit API" text="search and filter the top 25 SubReddits real time" projectLink="https://reddit-clone-v1.netlify.app/"/>
+                <ProjectCard img={countryAPIImg} title="NAME" text="Info on the project"/>
             </div>
         </div>
     )
